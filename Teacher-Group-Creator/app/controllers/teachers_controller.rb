cbf90d71620b3768578d68fbe23326e3070c15a2
Antony Adhiban & mertymcgraw & Brian Lee & Stacy <pair+antonyadhiban.mertymcgraw.solibl.stacy89@devbootcamp.com>
2017-05-10 12:03:04 -0700

    implement route#new for teacher registration

---
+++ Teacher-Group-Creator/app/controllers/teachers_controller.rb
@@ -0,0 +1,8 @@
+class TeachersController < ApplicationRecord
+
+def new
+  @teacher = Teacher.new
+end
+
+
+end
--- Teacher-Group-Creator/app/models/teacher.rb
+++ Teacher-Group-Creator/app/models/teacher.rb
@@ -3,5 +3,6 @@ class Teacher < ApplicationRecord
 
   validates :name, :email, :admin_status, :password, {presence: true}
   validates :email, {uniqueness: true}
+  validates :admin_status, {default: 2}
   has_secure_password
 end
