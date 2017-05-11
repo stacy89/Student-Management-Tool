f5cecb4d232a8831017bbc11cae89ca5f7ebddb8
Antony Adhiban & mertymcgraw & Brian Lee & Stacy <pair+antonyadhiban.mertymcgraw.solibl.stacy89@devbootcamp.com>
2017-05-10 19:12:54 -0700

    implement edit/update-- can update any student

--- Teacher-Group-Creator/app/controllers/students_controller.rb
+++ Teacher-Group-Creator/app/controllers/students_controller.rb
@@ -7,8 +7,32 @@ class StudentsController < ApplicationController
     @student = Student.find(params[:id])
   end
 
+  def edit
+    @student = Student.find(params[:id])
+  end
+
+  def update
+    @student = Student.find(params[:id])
+    @student.assign_attributes(student_params)
+
+    if @student.save
+      redirect_to root_path
+    else
+      @errors = @student.errors.full_messages
+      render 'edit'
+    end
+  end
+
   def nonpicked
     @student_nonpicked = Student.where(teacher_id: nil)
   end
+
+  private
+
+  def student_params
+     params.require(:student).permit(:name, :phase, :gender, :gpa, :num_detentions, :shirt_size, :dietary_restrictions, :emergency_contact_name, :emergency_contact_num)
+  end
+
+
 
 end
