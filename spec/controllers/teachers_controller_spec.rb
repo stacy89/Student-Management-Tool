b3910115eb8581cdb805f192bff1026129edfd4e
mertymcgraw & Brian Lee <pair+mertymcgraw.solibl@devbootcamp.com>
2017-05-11 10:11:41 -0700

    Add first teacher controller test

--- spec/controllers/teachers_controller_spec.rb
+++ spec/controllers/teachers_controller_spec.rb
@@ -1,1 +1,17 @@
-let!(:teacher) {Teacher.create(name: "Hagrid", email: "bob@bob.com", admin_status: 1, password_digest: "password" )}
+require 'rails_helper'
+
+describe TeachersController do
+  let!(:teacher) {Teacher.create!(name: "Hagrid", email: "bob@bob.com", admin_status: 1, password: "password" )}
+
+  describe "GET #index" do
+    it "responds with status code 200" do
+      get :index
+      expect(response).to have_http_status 200
+    end
+
+    it "assigns the all teachers as @teachers" do
+      get :index
+      expect(assigns(:teachers)).to eq Teacher.all
+    end
+  end
+end
